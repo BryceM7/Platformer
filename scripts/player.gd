2f307242dc8478dfaf3db47f19e5d8a94e30a7d4
BryceM7 <brycem417@gmail.com>
2024-09-05 13:22:49 -0400

    Double jump
    Added double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+var jump_count = 0
+var max_jumps = 2
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,10 +13,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+	if is_on_floor():
+		jump_count = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction: -1, 0, 1 
 	var direction = Input.get_axis("move_left", "move_right")
