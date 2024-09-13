9c28f5ec79baec1225f0789b8bd39b055b0d9021
BryceM7 <brycem417@gmail.com>
2024-09-13 19:51:47 -0400

    Main menu added

--- main_menu.gd
+++ main_menu.gd
@@ -16,4 +16,4 @@ func _on_exit_pressed():
 	get_tree().quit()
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scences/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
